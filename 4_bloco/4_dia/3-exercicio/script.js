6ef90a78b99c2b6aab87e5651cedba327dfc3e39
AlexandreRocha-10 <alexandre.e.rocha@gmail.com>
2022-08-23 19:22:14 -0300

    Exercicios dia 4 - bloco 4

---
+++ 4_bloco/4_dia/3-exercicio/script.js
@@ -0,0 +1,20 @@
+function sum (a, b) {
+  return a + b;
+}
+function sub(a, b) {
+  return a - b;
+}
+
+function mult(a, b) {
+  return a * b;
+}
+
+function div(a, b) {
+  return a / b;
+}
+
+function mod(a, b) {
+  return a % b;
+}
+
+console.log(sum(4,8));
