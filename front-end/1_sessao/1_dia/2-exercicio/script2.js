f30d7dd80e9f56026c150d479185aeebdf9511c4
AlexandreRocha-10 <alexandre.e.rocha@gmail.com>
2022-10-06 19:18:30 -0300

    Alguns exercicios do primeiro dia

---
+++ front-end/1_sessao/1_dia/2-exercicio/script2.js
@@ -0,0 +1,31 @@
+const messageDelay = () => Math.floor(Math.random() * 5000);
+
+const getMarsTemperature = () => {
+  const maxTemperature = 58;
+  return Math.floor(Math.random() * maxTemperature);
+};
+
+const toFahrenheit = (degreeCelsius) => (degreeCelsius * (9 / 5)) + 32;
+
+const temperatureInFahrenheit = (temperature) =>
+  console.log(`Atualmente está ${toFahrenheit(temperature)}ºF em Marte`);
+
+const greet = (temperature) =>
+  console.log(`Olá! Curiosity aqui. Nesse momento está ${temperature}ºC em Marte`);
+
+// Definição da função sendMarsTemperature...
+const sendMarsTemperature = (onSuccess, onError) => {
+    const currentTemperature = getMarsTemperature();
+    const messageSuccessfullySent = Math.random() <= 0.6;
+    setTimeout(() => {
+        if(messageSuccessfullySent) onSuccess(currentTemperature)
+        else onError('Robot is busy');
+    }, messageDelay());
+  }
+
+const error = (erro) => console.log(`Error getting temperature: ${erro}`);
+
+
+console.log(sendMarsTemperature(temperatureInFahrenheit, error)); // Imprime "Atualmente está 46.4ºF em Marte", por exemplo
+
+console.log(sendMarsTemperature(greet, error)); // Imprime "Olá! Curiosity aqui. Nesse momento são 36ºC em Marte", por exemplo
