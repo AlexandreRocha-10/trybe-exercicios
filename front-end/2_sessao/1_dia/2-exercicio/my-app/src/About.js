050fbf9d513357f6a010fc76c6d0d002adbc333b
AlexandreRocha-10 <alexandre.e.rocha@gmail.com>
2022-10-19 19:09:16 -0300

    Exercicio introd ao react

---
+++ front-end/2_sessao/1_dia/2-exercicio/my-app/src/About.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+class About extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>
+          Alexandre Rocha'
+        </h1>
+        <p>
+          Minha descrição
+        </p>
+        <ul>
+          <li>Minha Habilidade 01</li>
+          <li>Minha Habilidade 02</li>
+          <li>Minha Habilidade 03</li>                    
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default About;
