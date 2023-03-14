70c70aa83a31d3b89e981bb8702f5e06809f88aa
AlexandreRocha-10 <alexandre.e.rocha@gmail.com>
2023-03-14 16:10:26 -0300

    feat: alguns exercicios de testes em node

--- back-end/sessao_4/trybecash-api/src/server.js
+++ back-end/sessao_4/trybecash-api/src/server.js
@@ -1,10 +1,11 @@
 const app = require('./app');
 const connection = require('./db/connection');
 
-const PORT = 3012;
+const PORT = 3021;
 
 app.listen(PORT, async () => {
   console.log(`API TrybeCash está sendo executada na porta ${PORT}`);
+  console.log(`Valor da variável de ambiente $USER: ${process.env.USER}`);
 
   // O código abaixo é para testarmos a comunicação com o MySQL
   const [result] = await connection.execute('SELECT 1');
