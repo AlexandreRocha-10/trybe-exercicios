43fa13c246ebf92d6002ad8f404aa2b57ea7f68f
AlexandreRocha-10 <alexandre.e.rocha@gmail.com>
2023-04-11 17:40:57 -0300

    feat: exercicios de ORM Sequelize

---
+++ books/src/app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+
+const BookController = require('./controllers/BookController');
+
+const app = express();
+
+app.use(express.json());
+
+app.get('/books', BookController.getAll);
+app.get("/books/:id", BookController.getById);
+app.post('/books', BookController.create);
+app.put('/books/:id', BookController.update);
+app.delete('/books/:id', BookController.remove);
+
+module.exports = app;
