12185255ad1f7df463a4bc80da786c4fd8cdd03b
AlexandreRocha-10 <alexandre.e.rocha@gmail.com>
2023-04-25 17:25:29 -0300

    feat: exercicios msc

--- back-end/employees/src/services/employee.service.js
+++ back-end/employees/src/services/employee.service.js
@@ -1,6 +1,12 @@
 // src/services/employee.service.js
 
 const { Address, Employee } = require('../models/');
+const Sequelize = require('sequelize');
+const config = require('../config/config');
+
+const env = process.env.NODE_ENV || 'development';
+// Ajustamos para usar a configuração correta para nosso ambiente
+const sequelize = new Sequelize(config[env]);
 
 const getAll = async () => {
   const users = await Employee.findAll({
@@ -17,5 +23,19 @@ const getById = async (id) => {
     });
   return employee;
 }
+const insert = async ({ firstName, lastName, age, city, street, number }) => {
+  const result = await sequelize.transaction(async (t) => {
+    const employee = await Employee.create({
+      firstName, lastName, age,
+    }, { transaction: t });
 
-module.exports = { getAll, getById };
+    await Address.create({
+      city, street, number, employeeId: employee.id
+    }, { transaction: t });
+    return employee;
+  });
+
+  return result;
+};
+
+module.exports = { getAll, getById, insert };
