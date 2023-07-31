5958294c54b3af1de41ffb7d5582a12c447af5cd
AlexandreRocha-10 <alexandre.e.rocha@gmail.com>
2023-07-31 15:49:37 -0300

    exercicios de python

---
+++ python/src/produto.py
@@ -0,0 +1,18 @@
+class Produto:
+    def __init__(self, preco: float):
+        self.preco = float(preco)
+
+    def imprimir_preco(self):
+        raise NotImplementedError(
+            "Classes derivadas de Produto precisam implementar o método imprimir_preco."
+        )
+    
+class Livro(Produto):
+    def __init__(self, preco):
+        super().__init__(preco=preco)
+
+    def imprimir_preco(self):
+        print(f"O preço do livro é: {self.preco}")
+
+livro = Livro(10)
+livro.imprimir_preco()
---
+++ python/src/protocol.py
@@ -0,0 +1,31 @@
+from typing import Protocol
+
+
+class CalculaPerimetro(Protocol):
+    def calcular_perimetro(self):
+        pass
+
+
+class Quadrado(CalculaPerimetro):
+    def __init__(self, lado: int) -> None:
+        self.lado = lado
+
+    def calcular_perimetro(self):
+        return f"O perímetro do quadrado é de {self.lado * 4} cm"
+    
+
+class Triangulo(CalculaPerimetro):
+    def __init__(self, lado1: int, lado2: int, lado3: int) -> None:
+        self.lado1 = lado1
+        self.lado2 = lado2
+        self.lado3 = lado3
+
+    def calcular_perimetro(self):
+        return f"O perímetro do triângulo é de {self.lado1 + self.lado2 + self.lado3} cm"
+
+
+quadrado = Quadrado(5)
+print(quadrado.calcular_perimetro())
+
+triangulo = Triangulo(5, 7, 5)
+print(triangulo.calcular_perimetro())
